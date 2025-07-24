79ad1a513b20e80545fd64df6a0c5b94aeff2681
somka890 <somkiuks@gmail.com>
2025-07-24 21:55:11 +0300

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,84 +1,43 @@
-from flask import request, redirect, url_for, flash, render_template, Blueprint
-from flask_login import login_user, login_required, logout_user, current_user
-from datetime import datetime
-from .models import Produktas, Vartotojas, Irasas
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from .models import Vartotojas
 from . import db
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
-@main.route('/login', methods=['GET', 'POST'])
+@main.route("/")
+def index():
+    return redirect(url_for("main.login"))
+
+@main.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        el_pastas = request.form.get('el_pastas')
-        slaptazodis = request.form.get('slaptazodis')
+    if request.method == "POST":
+        el_pastas = request.form["el_pastas"]
+        slaptazodis = request.form["slaptazodis"]
 
         vart = Vartotojas.query.filter_by(el_pastas=el_pastas).first()
 
         if vart and vart.slaptazodis == slaptazodis:
             login_user(vart)
-            return redirect(url_for('main.records'))
-        flash('Neteisingi prisijungimo duomenys', 'danger')
+            return redirect(url_for("main.irasai"))
+        else:
+            flash("Neteisingas el. paštas arba slaptažodis.")
 
-    return render_template('login.html')
+    return render_template("login.html")
 
-@main.route('/logout')
+@main.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.login'))
+    return redirect(url_for("main.login"))
 
-@main.route('/irasai')
+@main.route("/irasai")
 @login_required
-def records():
-    page = request.args.get('page', 1, type=int)
+def irasai():
+    return render_template("irasai.html")
 
-    query = Irasas.query.filter_by(vartotojas_id=current_user.id).order_by(Irasas.data.desc())
-    visi_irasai = query.paginate(page=page, per_page=5)
-
-    return render_template('irasai.html', visi_irasai=visi_irasai, datetime=datetime)
-
-@main.route('/produktai')
-def produktai():
-    page = request.args.get('page', 1, type=int)
-    visi_produktai = Produktas.query.order_by(Produktas.pavadinimas).paginate(page=page, per_page=4)
-    return render_template('produktai.html', visi_produktai=visi_produktai)
-
-@main.route('/new-record')
-@login_required
-def new_record():
-    ir = Irasas(suma=123, pajamos=True, vartotojas_id=current_user.id)
-    db.session.add(ir)
-    db.session.commit()
-    flash('Irasas sukurtas (demo forma)', 'success')
-    return redirect(url_for('main.records'))
-
-# Route for testing
-@main.route("/uzpildyti-produktus")
-def uzpildyti_produktus():
-    pavadinimai = ["Obuolys", "Bananas", "Vanduo", "Sūris", "Duona", "Pomidoras", "Agurkas", "Pienas",
-                   "Mėsa", "Kava", "Arbata", "Sviestas", "Jogurtas", "Apelsinas", "Ryžiai", "Makaronai",
-                   "Saldainiai", "Druska", "Cukrus", "Šokoladas", "Kiaušiniai", "Faršas", "Limonadas",
-                   "Džiūvėsiai", "Čipsai", "Kukurūzai", "Tortas", "Majonezas", "Ketchupas", "Aliejus"]
-    for i, p in enumerate(pavadinimai):
-        produktas = Produktas(pavadinimas=p, kaina=round(1.0 + i * 0.5, 2))
-        db.session.add(produktas)
-    db.session.commit()
-    return "Produktai pridėti"
-
-
-
-@main.route('/init-db')
+# Šitas route prideda duomenų bazės lenteles
+@main.route("/init-db")
 def init_db():
-    from . import db
     db.create_all()
-    return "✅ DB lentelės sukurtos"
-
-@main.route('/add-test-user')
-def add_test_user():
-    from .models import Vartotojas
-    from . import db
-    vartotojas = Vartotojas(el_pastas="admin@admin.com", slaptazodis="admin")
-    db.session.add(vartotojas)
-    db.session.commit()
-    return "✅ Testinis vartotojas pridėtas"
-
+    return "DB sukurta"
